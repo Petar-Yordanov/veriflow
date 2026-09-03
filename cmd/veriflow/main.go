e4fac9a8e5ee17fe2bc2f9717d920215abac8960
Petar Yordanov <peter.yordanov.dev@gmail.com>
2026-09-03 16:47:19 +0300

    Fix CI

---
+++ cmd/veriflow/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"veriflow/internal/cli"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	os.Exit(cli.RunContext(ctx, os.Args[1:]))
+}
